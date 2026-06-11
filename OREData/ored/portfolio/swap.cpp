c385d5d7a54b27731046e326dfb1f81db2140a69
Laurentiu <andrei.laurentiu@nib.int>
2026-06-11 15:58:27 +0300

    Patch: enable useXbsCurves for indexing

--- OREData/ored/portfolio/swap.cpp
+++ OREData/ored/portfolio/swap.cpp
@@ -120,7 +120,7 @@ void Swap::build(const QuantLib::ext::shared_ptr<EngineFactory>& engineFactory) 
     /* determine whether we have a xccy and whether to use the special xbs curves */
     isXCCY_ = isXCCY_ || currenciesWithIndexing.size() > 1;
 
-    bool useXbsCurves = isSimmEligibleXccySwap(legData_, settlement_);
+    bool useXbsCurves = isSimmEligibleXccySwap(legData_, settlement_) || currenciesWithIndexing.size() > 1;
 
     std::tie(notionalTakenFromLeg_, notional_, npvCurrency_, notionalCurrency_) = getSwapNpvAndNotionalInfo(legData_);
 
